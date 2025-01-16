08015ba0b68ae717717111a44922adb0a71a29a7
Crosslywere <ogboruj@outlook.com>
2025-01-16 10:04:29 +0100

    changed white spaces

--- Lighting/premake5.lua
+++ Lighting/premake5.lua
@@ -1,32 +1,32 @@
 project "Lighting"
     kind "ConsoleApp"
     language "C++"
-	cppdialect "C++20"
-	targetdir ("../bin/%{prj.name}")
-	objdir ("../obj/%{prj.name}")
-	files {
-		"src/*.h",
-		"src/*.cpp",
-		"../vendors/glad/src/glad.c",
-		"res/*.glsl",
-		"res/*.png",
+    cppdialect "C++20"
+    targetdir ("../bin/%{prj.name}")
+    objdir ("../obj/%{prj.name}")
+    files {
+        "src/*.h",
+        "src/*.cpp",
+        "../vendors/glad/src/glad.c",
+        "res/*.glsl",
+        "res/*.png",
         "res/*.jpg",
-	}
-	includedirs {
+    }
+    includedirs {
         "%{IncludeDirs.GLFW}",
-		"%{IncludeDirs.GLAD}",
-		"%{IncludeDirs.STB}",
-		"%{IncludeDirs.GLM}",
+        "%{IncludeDirs.GLAD}",
+        "%{IncludeDirs.STB}",
+        "%{IncludeDirs.GLM}",
         "../include",
-	}
-	vpaths {
-		["Source Files"] = { "**.cpp", "**.c" },
-		["Header Files"] = "**.h",
+    }
+    vpaths {
+        ["Source Files"] = { "**.cpp", "**.c" },
+        ["Header Files"] = "**.h",
         ["Resource Files"] = { "**.glsl", "**.png", "**.jpg" },
-	}
-	libdirs {
-		"../bin/libs",
-	}
-	links {
-		"glfw",
-	}
+    }
+    libdirs {
+        "../bin/libs",
+    }
+    links {
+        "glfw",
+    }
