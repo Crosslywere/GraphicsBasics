b672f8631430d3215b2d09ed13d42b929a1398f6
Crosslywere <ogboruj@outlook.com>
2025-01-16 05:25:37 +0100

    implemented shader structs for lights

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -23,17 +23,47 @@ static void extractTextFromFile(const std::string& file, std::string& store) {
     }
 }
 
+namespace phong {
+    struct LightSource {
+        glm::vec3 ambient;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
+    };
+    struct DirectionalLight : public LightSource {
+        glm::vec3 direction;
+    };
+    struct PointLight : public LightSource {
+        glm::vec3 position;
+
+        float constant = 1.f;
+        float linear;
+        float quadratic;
+    };
+    typedef struct SpotLight : public LightSource {
+        glm::vec3 position;
+        glm::vec3 direction;
+        float innerCutoff;
+        float outerCutoff;
+        float constant = 1.f;
+        float linear;
+        float quadratic;
+    } FlashLight;
+}
 // The shader class
 class Shader {
 public:
 	~Shader() {
 		glDeleteShader(m_ProgramObject);
 	}
-	// Loading from memory uses the parameters as the shader's source code
+	/// <summary>Loading from memory uses the parameters as the shader's source code</summary>
+    /// <param name="vertexSource">Source code for the vertex shader</param>
+    /// <param name="fragmentSource">Source code for the fragment shader</param>
 	static Shader CreateFromSource(const char* vertexSource, const char* fragmentSource) {
 		return Shader(vertexSource, fragmentSource);
 	}
-	// Loading from file extracts the contents of the file to use as the shader's source code
+	/// <summary>Loading from file extracts the contents of the file to use as the shader's source code</summary>
+    /// <param name="vertexFile">File path to vertex shader source code file</param>
+    /// <param name="fragmentFile">File path to fragment shader source code file</param>
 	static Shader LoadFromFile(const char* vertexFile, const char* fragmentFile) {
 		std::string vertexSource;
 		std::string fragmentSource;
@@ -74,6 +104,93 @@ public:
 			glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(m));
 		}
 	}
+    /// Sets variables for a struct in the shader:
+    /// struct {
+    ///     vec3 ambient;
+    ///     vec3 diffuse;
+    ///     vec3 specular;
+    /// };
+    /// Used like 'name.diffuse' where name is the string name parameter
+    void SetLightColors(const std::string& name, const phong::LightSource& light) {
+        SetFloat3((name + ".ambient").c_str(), light.ambient);
+        SetFloat3((name + ".diffuse").c_str(), light.diffuse);
+        SetFloat3((name + ".specular").c_str(), light.specular);
+    }
+    /// Sets variable for a struct in the shader:
+    /// struct {
+    ///     vec3 ambient;
+    ///     vec3 diffuse;
+    ///     vec3 specular;
+    ///     vec3 direction;
+    /// };
+    /// Used like 'name.direction' where name is the string name parameter
+    void SetLight(const std::string& name, const phong::DirectionalLight& light) {
+        SetLightColors(name, light);
+        SetFloat3((name + ".direction").c_str(), light.direction);
+    }
+    void SetLights(const std::string& name, const phong::DirectionalLight* lights, size_t size) {
+        for (size_t i = 0; i < size; i++) {
+            std::stringstream ss;
+            ss << name << '[' << i << ']';
+            SetLight(ss.str(), lights[i]);
+        }
+    }
+    /// Sets variable for a struct in the shader:
+    /// struct {
+    ///     vec3 ambient;
+    ///     vec3 diffuse;
+    ///     vec3 specular;
+    ///     vec3 position;
+    ///     float constant;
+    ///     float linear;
+    ///     float quadratic;
+    /// };
+    /// Used like 'name.quadratic' where name is the string name parameter
+    void SetLight(const std::string& name, const phong::PointLight& light) {
+        SetLightColors(name, light);
+        SetFloat3((name + ".position").c_str(), light.position);
+        SetFloat((name + ".constant").c_str(), light.constant);
+        SetFloat((name + ".linear").c_str(), light.linear);
+        SetFloat((name + ".quadratic").c_str(), light.quadratic);
+    }
+    void SetLights(const std::string& name, const phong::PointLight* lights, size_t size) {
+        for (size_t i = 0; i < size; i++) {
+            std::stringstream ss;
+            ss << name << '[' << i << ']';
+            SetLight(ss.str(), lights[i]);
+        }
+    }
+    /// Sets vrauble for a struct in the shader:
+    /// struct {
+    ///     vec3 ambient;
+    ///     vec3 diffuse;
+    ///     vec3 specular;
+    ///     vec3 direction;
+    ///     vec3 position;
+    ///     float innerCutoff;
+    ///     float outerCutoff;
+    ///     float constant;
+    ///     float linear;
+    ///     float quadratic;
+    /// };
+    /// Used like 'name.cutoff' where name is the string name parameter
+    void SetLight(const std::string& name, const phong::SpotLight& light) {
+        SetLightColors(name, light);
+        SetFloat3((name + ".direction").c_str(), light.direction);
+        SetFloat3((name + ".position").c_str(), light.position);
+        SetFloat((name + ".innerCutoff").c_str(), light.innerCutoff);
+        SetFloat((name + ".outerCutoff").c_str(), light.outerCutoff);
+        SetFloat((name + ".constant").c_str(), light.constant);
+        SetFloat((name + ".linear").c_str(), light.linear);
+        SetFloat((name + ".quadratic").c_str(), light.quadratic);
+    }
+    void SetLights(const std::string& name, const phong::FlashLight* lights, size_t size) {
+        for (size_t i = 0; i < size; i++) {
+            std::stringstream ss;
+            ss << name << '[' << i << ']';
+            SetLight(ss.str(), lights[i]);
+        }
+    }
 private:
 	// Shader constructor
 	Shader(const char* vertexSource, const char* fragmentSource) {
